95547dd692667b3a952aa6e0930f2f8bf2f7367f
Nikolas G <gianick@users.noreply.github.com>
2019-01-24 16:04:46 +0000

    BTA-1135 Replaced "unauthorised" page with form - WIP

--- app/controllers/UnauthorisedController.scala
+++ app/controllers/UnauthorisedController.scala
@@ -17,17 +17,47 @@
 package controllers
 
 import javax.inject.Inject
-
 import config.FrontendAppConfig
+import models.VatNotAddedFormModel
+import play.api.data
+import play.api.data.Form
+import play.api.data.Forms.mapping
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 import views.html.unauthorised
+import play.api.data.Form
+import play.api.data.Forms._
+
+import scala.concurrent.Future
+
 
 class UnauthorisedController @Inject()(val appConfig: FrontendAppConfig,
                                        val messagesApi: MessagesApi) extends FrontendController with I18nSupport {
 
+  val vatNotAddedForm: Form[VatNotAddedFormModel] = Form(
+    mapping(
+      "radioOption" -> nonEmptyText
+    )(VatNotAddedFormModel.apply)(VatNotAddedFormModel.unapply)
+  )
+
   def onPageLoad: Action[AnyContent] = Action { implicit request =>
-    Unauthorized(unauthorised(appConfig))
+    Unauthorized(views.html.unauthorised(vatNotAddedForm, appConfig))
   }
+
+  def processForm: Action[AnyContent] = Action { implicit request =>
+    vatNotAddedForm.bindFromRequest.fold(
+      (formWithErrors: Form[VatNotAddedFormModel]) => {
+        BadRequest(views.html.unauthorised(formWithErrors, appConfig))
+      },
+      (formData: VatNotAddedFormModel) => {
+        formData.radioOption match {
+          case "sign_in_to_other_account" => Redirect(appConfig.businessAccountWrongCredsUrl)
+          case "add_your_vat_to_this_account" => Redirect(appConfig.addVatUrl)
+        }
+      }
+    )
+  }
+
+
 }
