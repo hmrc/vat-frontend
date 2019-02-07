0bfc9832027790af128210ccebc3d31d1b818e59
Alex Newby <41003702+alex-newby-hmrc@users.noreply.github.com>
2019-02-07 14:34:14 +0000

    Make controller pass amount in pence rather than pounds

--- app/controllers/PaymentStartController.scala
+++ app/controllers/PaymentStartController.scala
@@ -17,27 +17,41 @@
 package controllers
 
 import config.FrontendAppConfig
+import connectors.models.{AccountBalance, AccountSummaryData, VatData}
 import connectors.payments.{PayConnector, SpjRequestBtaVat, VatPeriod}
 import controllers.actions._
 import javax.inject.Inject
 import play.api.mvc.{Action, AnyContent}
+import services.VatService
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import PaymentStartController.toAmountInPence
 
 import scala.concurrent.ExecutionContext
+
+object PaymentStartController {
+  def toAmountInPence(amountInPounds: BigDecimal): Long = (amountInPounds * 100).toLong
+}
 
 class PaymentStartController @Inject()(appConfig: FrontendAppConfig,
                                        payConnector: PayConnector,
                                        authenticate: AuthAction,
+                                       vatService: VatService,
                                        serviceInfo: ServiceInfoAction)(implicit ec: ExecutionContext) extends FrontendController {
 
   def makeAPayment: Action[AnyContent] = (authenticate andThen serviceInfo).async {
     implicit request =>
-      val spjRequestBtaVat = SpjRequestBtaVat(
-        3,
-        appConfig.businessAccountHomeUrl,
-        appConfig.businessAccountHomeUrl,
-        VatPeriod(1, 2),
-        request.request.vatDecEnrolment.vrn.vrn)
-      payConnector.vatPayLink(spjRequestBtaVat).map(response => Redirect(response.nextUrl))
+      vatService.fetchVatModel(Some(request.request.vatDecEnrolment)).flatMap {
+        case VatData(AccountSummaryData(Some(AccountBalance(Some(amount))), _, openPeriods), _) =>
+          val spjRequestBtaVat = SpjRequestBtaVat(
+            toAmountInPence(amount),
+            appConfig.businessAccountHomeUrl,
+            appConfig.businessAccountHomeUrl,
+            VatPeriod(1, 2), //openPeriods.minBy(_.openPeriod),
+            request.request.vatDecEnrolment.vrn.vrn)
+          payConnector.vatPayLink(spjRequestBtaVat).map(response => Redirect(response.nextUrl))
+
+        case _ => throw new Exception("")
+      }
+
   }
 }
