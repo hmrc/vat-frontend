55424f3fec0cd809bd3e5711e15c6882f40cb268
Alex Newby <41003702+alex-newby-hmrc@users.noreply.github.com>
2019-02-11 10:13:43 +0000

    Remove unnecessary comments from connector test

--- test/connectors/PayConnectorSpec.scala
+++ test/connectors/PayConnectorSpec.scala
@@ -17,12 +17,9 @@
 package connectors
 
 import base.SpecBase
-import connectors.models.designatorydetails.{DesignatoryDetails, DesignatoryDetailsCollection, DesignatoryDetailsName}
-import connectors.models.{AccountBalance, AccountSummaryData, CalendarData, _}
 import connectors.payments.{NextUrl, PayConnector, SpjRequestBtaVat, VatPeriod}
-import org.joda.time.LocalDate
 import org.mockito.Matchers
-import org.mockito.Mockito.{verify, when}
+import org.mockito.Mockito.when
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mockito.MockitoSugar
 import play.api.http.Status._
@@ -75,92 +72,5 @@ class PayConnectorSpec extends SpecBase with MockitoSugar with ScalaFutures with
         }
       }
     }
-
-//    "designatoryDetails is called" should {
-//
-//      "return valid DesignatoryDetailsCollection" in {
-//
-//        val designatoryDetailsCollection = DesignatoryDetailsCollection(
-//          Some(DesignatoryDetails(DesignatoryDetailsName(nameLine1 = Some("name1"), nameLine2 = Some("name2"))))
-//        )
-//
-//        val response = vatConnector(
-//          mockedResponse = HttpResponse(OK, Some(Json.toJson(designatoryDetailsCollection)))
-//        ).designatoryDetails(vrn)
-//
-//        whenReady(response) {
-//          r =>
-//            r mustBe Some(designatoryDetailsCollection)
-//        }
-//      }
-//
-//      "return 404 if nothing is returned" in {
-//        val response = vatConnector(
-//          mockedResponse = HttpResponse(NOT_FOUND, None)
-//        ).designatoryDetails(vrn)
-//
-//        whenReady(response) { r =>
-//          r mustBe None
-//        }
-//      }
-//
-//      "return MicroServiceException if response couldn't be mapped" in {
-//        val vatAccountSummaryUri = "http://localhost:8880/vat/vrn/designatoryDetails"
-//        val httpWrapper = mock[HttpWrapper]
-//
-//        val response = vatConnector(
-//          mockedResponse = HttpResponse(INTERNAL_SERVER_ERROR, None),
-//          httpWrapper
-//        ).designatoryDetails(vrn)
-//
-//        whenReady(response.failed) { mse =>
-//          mse mustBe a[MicroServiceException]
-//          verify(httpWrapper).getF[AccountSummaryData](vatAccountSummaryUri)
-//        }
-//
-//      }
-//
-//    }
-
-//    "calender is called" should {
-//
-//      "return valid calenderData" in {
-//        val vatCalender = CalendarData(
-//          Some("0000"), DirectDebit(true, None), None, Seq(CalendarPeriod(new LocalDate("2018-04-02"), new LocalDate("2019-04-02"), None, true))
-//        )
-//        val response = vatConnector(
-//          mockedResponse = HttpResponse(OK, Some(Json.toJson(vatCalender)))).calendar(vrn)
-//
-//        whenReady(response) {
-//          r =>
-//            r mustBe Some(vatCalender)
-//        }
-//      }
-//
-//      "return 404 if nothing is returned" in {
-//        val response = vatConnector(
-//          mockedResponse = HttpResponse(NOT_FOUND, None)
-//        ).calendar(vrn)
-//
-//        whenReady(response) { r =>
-//          r mustBe None
-//        }
-//      }
-//
-//      "return MicroServiceException if response couldn't be mapped" in {
-//        val vatCalendarUri = "http://localhost:8880/vat/vrn/calendar"
-//        val httpWrapper = mock[HttpWrapper]
-//
-//        val response = vatConnector(
-//          mockedResponse = HttpResponse(INTERNAL_SERVER_ERROR, None),
-//          httpWrapper
-//        ).calendar(vrn)
-//
-//        whenReady(response.failed) { mse =>
-//          mse mustBe a[MicroServiceException]
-//          verify(httpWrapper).getF[CalendarData](vatCalendarUri)
-//        }
-//      }
-//    }
   }
 }
