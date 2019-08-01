e005a67db19390e0a5918571794e4fee618575b8
Andrew Shanks <18327122+AndrewShanks@users.noreply.github.com>
2019-08-01 17:18:25 +0100

    BTA-1578: Format of date from user enrolment matches format in real data

--- test/connectors/EnrolmentStoreConnectorSpec.scala
+++ test/connectors/EnrolmentStoreConnectorSpec.scala
@@ -16,8 +16,8 @@
 
 package connectors
 
+import _root_.models.{UserEnrolmentStatus, UserEnrolments}
 import base.SpecBase
-import _root_.models.{UserEnrolments, UserEnrolmentStatus}
 import org.joda.time.DateTime
 import org.mockito.Matchers
 import org.mockito.Mockito.when
@@ -28,15 +28,15 @@ import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, Upstream5xxResponse}
 import uk.gov.hmrc.play.bootstrap.http.HttpClient
 
 import scala.concurrent.ExecutionContext.Implicits.global
-
 import scala.concurrent.Future
 
-class EnrolmentStoreConnectorSpec  extends SpecBase with MockitoSugar with ScalaFutures with MockHttpClient {
+class EnrolmentStoreConnectorSpec extends SpecBase with MockitoSugar with ScalaFutures with MockHttpClient {
   implicit val hc = HeaderCarrier()
 
   val httpGet = mock[HttpClient]
 
-  val connector = new EnrolmentStoreConnectorImpl(httpGet,frontendAppConfig)
+  val connector = new EnrolmentStoreConnectorImpl(httpGet, frontendAppConfig)
+
   def result = connector.getEnrolments("cred-id")
 
   "EnrolmentStoreConnectorSpec" when {
@@ -50,12 +50,12 @@ class EnrolmentStoreConnectorSpec  extends SpecBase with MockitoSugar with Scala
               |{
               |"service":"IR-PAYE",
               |"state":"active",
-              |"enrolmentTokenExpiryDate":"2018-10-13T17:36:00.000Z"
+              |"enrolmentTokenExpiryDate":"2018-10-13 17:36:00.000"
               |}]
               |}
             """.stripMargin))))
         )
-        result.futureValue mustBe  Right(UserEnrolments(List(UserEnrolmentStatus("IR-PAYE", Some("active"), Some(new DateTime("2018-10-13T17:36:00.000Z").toLocalDateTime)))))
+        result.futureValue mustBe Right(UserEnrolments(List(UserEnrolmentStatus("IR-PAYE", Some("active"), Some(new DateTime("2018-10-13T17:36:00.000").toLocalDateTime)))))
       }
       "handle a 200 response with multiple enrolments" in {
         when(httpGet.GET[HttpResponse](Matchers.any())(Matchers.any(), Matchers.any(), Matchers.any())).thenReturn(
@@ -66,23 +66,23 @@ class EnrolmentStoreConnectorSpec  extends SpecBase with MockitoSugar with Scala
               |{
               |"service":"IR-PAYE",
               |"state":"active",
-              |"enrolmentTokenExpiryDate":"2018-10-13T17:36:00.000Z"
+              |"enrolmentTokenExpiryDate":"2018-10-13 17:36:00.000"
               |},
               |{"service":"VAT",
               |"state":"active",
-              |"enrolmentTokenExpiryDate":"2018-10-13T17:36:00.000Z"
+              |"enrolmentTokenExpiryDate":"2018-10-13 17:36:00.000"
               |},
               |{"service":"SA",
               |"state":"active",
-              |"enrolmentTokenExpiryDate":"2018-10-13T17:36:00.000Z"
+              |"enrolmentTokenExpiryDate":"2018-10-13 17:36:00.000"
               |}
               |]
               |}
             """.stripMargin))))
         )
-        result.futureValue mustBe Right(UserEnrolments(List(UserEnrolmentStatus("IR-PAYE", Some("active"), Some(new DateTime("2018-10-13T17:36:00.000Z").toLocalDateTime)),
-          UserEnrolmentStatus("VAT", Some("active"), Some(new DateTime("2018-10-13T17:36:00.000Z").toLocalDateTime)),
-          UserEnrolmentStatus("SA", Some("active"), Some(new DateTime("2018-10-13T17:36:00.000Z").toLocalDateTime)))))
+        result.futureValue mustBe Right(UserEnrolments(List(UserEnrolmentStatus("IR-PAYE", Some("active"), Some(new DateTime("2018-10-13T17:36:00.000").toLocalDateTime)),
+          UserEnrolmentStatus("VAT", Some("active"), Some(new DateTime("2018-10-13T17:36:00.000").toLocalDateTime)),
+          UserEnrolmentStatus("SA", Some("active"), Some(new DateTime("2018-10-13T17:36:00.000").toLocalDateTime)))))
       }
       "handle a 200 response with invalid JSON" in {
         when(httpGet.GET[HttpResponse](Matchers.any())(Matchers.any(), Matchers.any(), Matchers.any())).thenReturn(
