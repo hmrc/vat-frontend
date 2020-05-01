3331441e139c2eaad11bf7237570802dffae848c
AndrewRButcher <6122384+AndrewRButcher@users.noreply.github.com>
2020-05-01 11:01:31 +0100

    Dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,16 +8,16 @@ private object AppDependencies {
     ws,
     "com.typesafe.play" %% "play-json" % "2.6.14",
     "com.typesafe.play" %% "play-json-joda" % "2.6.14",
-    "uk.gov.hmrc" %% "logback-json-logger" % "4.6.0",
-    "uk.gov.hmrc" %% "govuk-template" % "5.26.0-play-26",
-    "uk.gov.hmrc" %% "play-health" % "3.14.0-play-26",
-    "uk.gov.hmrc" %% "play-ui" % "8.7.0-play-26",
-    "uk.gov.hmrc" %% "http-caching-client" % "8.4.0-play-26",
+//    "uk.gov.hmrc" %% "logback-json-logger" % "4.8.0",
+    "uk.gov.hmrc" %% "govuk-template" % "5.54.0-play-26",
+//    "uk.gov.hmrc" %% "play-health" % "3.14.0-play-26",
+    "uk.gov.hmrc" %% "play-ui" % "8.9.0-play-26",
+    "uk.gov.hmrc" %% "http-caching-client" % "9.0.0-play-26",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.2.0-play-26",
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.5.0",
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.7.0",
     "uk.gov.hmrc" %% "play-language" % "4.2.0-play-26",
-    "uk.gov.hmrc" %% "play-partials" % "6.9.0-play-26",
-    "uk.gov.hmrc" %% "domain" % "5.6.0-play-26"
+    "uk.gov.hmrc" %% "play-partials" % "6.10.0-play-26",
+    "uk.gov.hmrc" %% "domain" % "5.8.0-play-26"
   )
 
   object Test {
@@ -27,9 +27,9 @@ private object AppDependencies {
       Seq(
         "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % scope,
         "org.pegdown" % "pegdown" % "1.6.0" % scope,
-        "org.jsoup" % "jsoup" % "1.11.3" % scope,
+        "org.jsoup" % "jsoup" % "1.13.1" % scope,
         "org.mockito" % "mockito-all" % "2.0.2-beta" % scope,
-        "org.scalacheck" %% "scalacheck" % "1.14.0" % scope
+        "org.scalacheck" %% "scalacheck" % "1.14.3" % scope
       )
   }
 
