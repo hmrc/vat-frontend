5da7042c462bb434cea52f5a6e8384957910fd7b
Arunan Kannan <103437165+arunankannan@users.noreply.github.com>
2024-06-06 16:59:45 +0100

    DL-12528 vat-frontend dependency upgrades

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sbt -Dlogger.resource=logback-test.xml -Dapplication.router="testOnlyDoNotUseInAppConf.Routes" "run 9732"
+sbt -Dlogger.resource=logback-test.xml -Dplay.http.router="testOnlyDoNotUseInAppConf.Routes" "run 9732"
